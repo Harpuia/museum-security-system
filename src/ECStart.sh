87c490875cfc140f3e83ebfff586bcf2a97832e6
Yazid Hamdi <yazid.hamdi@outlook.com>
2017-02-26 20:27:50 -0500

    Cleaned up the project

--- src/ECStart.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-java TemperatureController $1 &
-java HumidityController $1 &
-java TemperatureSensor $1 &
-java HumiditySensor $1 &
-java ECSConsole $1
--- src/EMStart.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rmiregistry &
-java MessageManager
