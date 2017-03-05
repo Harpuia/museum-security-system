a5d030417f79e909df68bed0deed3543fa968519
Yazid Hamdi <yazid.hamdi@outlook.com>
2017-03-04 19:52:19 -0500

    Changed it to make calling the send alive signal work on an independent thread in one simple call.

--- src/Extension/MaintainableDevice.java
+++ src/Extension/MaintainableDevice.java
@@ -6,28 +6,44 @@ import MessagePackage.MessageManagerInterface;
 /**
  * Created by yazid on 03-Mar-17.
  */
-public abstract class MaintainableDevice{
+public abstract class MaintainableDevice {
 
-    private boolean isStarted=false;
+    private boolean isStarted = false;
     private int msgID;
     private String text;
 
     //After registering to the message manager, call this method
     //The message format is "name:description"
     //Message ID = 3
-    public void SendAliveSignal(String name, String description, MessageManagerInterface em)
-    {
-        Message msg=new Message(3,name+" : "+description);
-        try {
-            if (!isStarted) {
-                while (true) {
-                    em.SendMessage(msg);
-                    isStarted=true;
-                    Thread.sleep(5000);
+    public void SendAliveSignal(String name, String description, MessageManagerInterface em) {
+        AliveThread aliveThread = new AliveThread(name, description, em);
+        aliveThread.start();
+    }
+
+    public class AliveThread extends Thread {
+        private String name;
+        private String description;
+        private MessageManagerInterface em;
+
+        public AliveThread(String name, String description, MessageManagerInterface em){
+            this.name = name;
+            this.description = description;
+            this.em = em;
+        }
+
+        public void run() {
+            Message msg = new Message(3, name + " : " + description);
+            try {
+                if (!isStarted) {
+                    while (true) {
+                        em.SendMessage(msg);
+                        isStarted = true;
+                        Thread.sleep(5000);
+                    }
                 }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        } catch(Exception e) {
-            e.printStackTrace();
         }
     }
 }
